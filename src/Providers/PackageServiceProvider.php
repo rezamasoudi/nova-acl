7db1e98b29586d47847ed1f27493278bb81d905f
Reza Masoudi <dev.masoudi@gmail.com>
2023-01-04 13:06:47 +0330

    add translate command

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -3,16 +3,22 @@
 namespace Masoudi\NovaAcl\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Masoudi\NovaAcl\Console\Translate;
 
 class PackageServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         if ($this->app->runningInConsole()) {
+
             $this->publishes([
                 __DIR__ . "/../../export/database/migrations" => database_path("migrations"),
                 __DIR__ . "/../../export/class/resources" => app_path("Nova")
             ], 'nova-acl');
+
+            $this->commands([
+                Translate::class,
+            ]);
         }
     }
 }
